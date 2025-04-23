b17882043875992ffb894298bd37c20dba2fe8b0
白染喵 <280872732@qq.com>
2025-04-23 17:27:28 +0800

    Initial commit

---
+++ apps/check.js
@@ -0,0 +1,77 @@
+import fs from 'fs'
+
+export class CheckClass extends plugin {
+  constructor () {
+    super({
+      name: '课程插件-查询课表',
+      dsc: '',
+      event: 'message',
+      rule: [
+        {
+          reg: '#?查询(明天|明日|今日|今天)课表',
+          fnc: 'checkClass'
+        }
+      ]
+    })
+  }
+
+  async checkClass (e) {
+    let DT = e.msg.replace(/#?查询/g, '').replace(/课表/g, '').trim()
+    let Class = ''
+    if (DT === '今天' || DT === '今日') {
+      Class = this.GetClass(e, 'today')
+    } else {
+      Class = this.GetClass(e, 'tomorrow')
+    }
+    e.reply(Class)
+  }
+
+  GetClass (e, day) {
+    let userid = e.user_id
+    const path = `./plugins/Course-Plugin/data/${userid}.json`
+    let now = new Date()
+    let dayOfWeek = now.getDay()
+    let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    if (day === 'today') {
+      day = days[dayOfWeek]
+    } else {
+      if (dayOfWeek === 6) {
+        day = days[0]
+      } else {
+        day = days[dayOfWeek + 1]
+      }
+    }
+    try {
+      const data = JSON.parse(fs.readFileSync(path, 'utf-8'))
+      if (!data[day] || Object.keys(data[day]).length === 0) {
+        return '没有找到课程哦~'
+      } else {
+        let reply = ''
+        const sortedKeys = Object.keys(data[day]).sort((a, b) => {
+          return parseInt(a, 10) - parseInt(b, 10)
+        })
+        for (const key of sortedKeys) {
+          let course = data[day][key]
+          const [S, E] = key.split('')
+          const startTime = data.time[S]
+          const endTime = this.addMinutesToTime(startTime, E * 60 - 10)
+          reply += `课程：${course.class}\n地点：${course.place}\n时间：${startTime} - ${endTime}\n\n`
+        }
+        return reply.trim()
+      }
+    } catch (error) {
+      logger.error('查询失败：', error)
+      return '查询失败，请检查配置文件：' + error.message
+    }
+  }
+
+  addMinutesToTime (time, minutes) {
+    const date = new Date()
+    const [hours, mins] = time.split(':').map(Number)
+    date.setHours(hours, mins, 0, 0)
+    date.setMinutes(date.getMinutes() + minutes)
+    const newHours = date.getHours().toString().padStart(2, '0')
+    const newMins = date.getMinutes().toString().padStart(2, '0')
+    return `${newHours}:${newMins}`
+  }
+}
---
+++ index.js
@@ -0,0 +1,85 @@
+import fs from 'fs/promises'
+import path from 'node:path'
+let AppName = 'course-plugin'
+const moduleCache = new Map()
+let loadedFilesCount = 0
+let loadedFilesCounterr = 0
+let apps
+if (!global.segment) {
+  const oicq = await import('oicq')
+  global.segment = oicq.segment
+}
+global.hl_plugin = {
+  apps,
+  puppeteer: null
+}
+const startTime = Date.now()
+logger.info('\x1b[36m' + AppName + '正在加载中...\x1b[0m')
+const { apps: loadedApps, loadedFilesCount: count, loadedFilesCounterr: counterr } = await appsOut({ AppsName: 'apps' })
+const endTime = Date.now()
+apps = loadedApps
+loadedFilesCount = count
+loadedFilesCounterr = counterr
+logger.info(logger.blue(`[${AppName}] 共加载了 ${loadedFilesCount} 个插件文件 ${loadedFilesCounterr} 个失败 耗时 ${endTime - startTime} 毫秒`))
+export { apps }
+
+async function appsOut ({ AppsName }) {
+  const firstName = path.join('plugins', AppName)
+  const filepath = path.resolve(firstName, AppsName)
+  let loadedFilesCount = 0
+  let loadedFilesCounterr = 0
+  const apps = {}
+
+  try {
+    const jsFilePaths = await traverseDirectory(filepath)
+    await Promise.all(jsFilePaths.map(async (item) => {
+      try {
+        const allExport = moduleCache.has(item)
+          ? moduleCache.get(item)
+          : await import(`file://${item}`)
+
+        for (const key of Object.keys(allExport)) {
+          if (typeof allExport[key] === 'function' && allExport[key].prototype) {
+            let className = key
+            if (Object.prototype.hasOwnProperty.call(apps, className)) {
+              let counter = 1
+              while (Object.prototype.hasOwnProperty.call(apps, `${className}_${counter}`)) {
+                counter++
+              }
+              className = `${className}_${counter}`
+              logger.info(`[${AppName}] 同名导出 ${key} 重命名为 ${className} : ${item}`)
+            }
+            apps[className] = allExport[key]
+            loadedFilesCount++
+          }
+        }
+      } catch (error) {
+        logger.error(`[${AppName}] 加载 ${item} 文件失败: ${error.message}`)
+        loadedFilesCounterr++
+      }
+    }))
+  } catch (error) {
+    logger.error('读取插件目录失败:', error.message)
+  }
+
+  return { apps, loadedFilesCount, loadedFilesCounterr }
+}
+
+async function traverseDirectory (dir) {
+  try {
+    const files = await fs.readdir(dir, { withFileTypes: true })
+    const jsFiles = []
+    for await (const file of files) {
+      const pathname = path.join(dir, file.name)
+      if (file.isDirectory()) {
+        jsFiles.push(...await traverseDirectory(pathname))
+      } else if (file.name.endsWith('.js')) {
+        jsFiles.push(pathname)
+      }
+    }
+    return jsFiles
+  } catch (error) {
+    logger.error('读取插件目录失败:', error.message)
+    return []
+  }
+}
